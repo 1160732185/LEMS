b955c76749b4430119297db38e82e32ad7eff151
干子雄 <g64848066@gmail.com>
2019-06-20 09:39:12 +0800

    成功使用Swagger2图形ui界面

--- src/main/java/com/example/laboratory/web/security/SecurityConfig.java
+++ src/main/java/com/example/laboratory/web/security/SecurityConfig.java
@@ -7,7 +7,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/api/v1/**").permitAll()
+        http.authorizeRequests().antMatchers("/api/v1/**",
+                "/swagger-resources",
+                "/configuration/security",
+                "/swagger-resources",
+                "/v2/api-docs",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "/swagger-resources/configuration/ui").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .and().httpBasic();
